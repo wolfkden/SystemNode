45d60dce424ea8bd35bf9d254a240ba0745bc345
wolfkden <wolfkden@gmail.com>
2015-02-14 19:02:07 -0800

    First Commit

--- SNode/main.cpp
+++ SNode/main.cpp
@@ -7,9 +7,18 @@
 //
 
 #include <iostream>
+#include "BTree.h"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    Node<int> na(4);
+
+    std::list<int> testVals{7,6,11,8,13,17,19,23,29,2};
+    for(int val : testVals) {
+        na + Node<int>(val);
+        std::cout << " : " << na.totalLevels << " : " << na.totalCardinality << " : " << na.totalWeight << " : " << na.weightDiff() << " TN: " << na << std::endl;
+    }
+
+//    std::cout << "Hello, World!\n";
     return 0;
 }
