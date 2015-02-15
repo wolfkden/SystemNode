f2826001aa2ee4f6bc6125ebdc3c9861d83d2980
wolfkden <wolfkden@gmail.com>
2015-02-14 18:41:27 -0800

    Initial Commit

---
+++ SNode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SNode
+//
+//  Created by Wolfgang Kraske on 2/14/15.
+//  Copyright (c) 2015 Wolfgang Kraske. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
